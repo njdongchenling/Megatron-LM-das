dfe4e5c6c686d6ea789ab89dd4c661a7c1d84677
dongcl <791582849@qq.com>
2026-08-25 17:41:40 +0800

    Add copyright info

--- hcu_megatron/core/full_cuda_graph.py
+++ hcu_megatron/core/full_cuda_graph.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2026 Hygon Information Technology Co., Ltd.
 
 import torch
--- hcu_megatron/training/__init__.py
+++ hcu_megatron/training/__init__.py
@@ -1,1 +1,3 @@
+# Copyright (c) 2026 Hygon Information Technology Co., Ltd.
+
 from .arguments import get_args
